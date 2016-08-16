d6616c255aed9c22f486ebb2a6d86e6870b35e8c
Віталій Палюшок < >
2016-08-16 13:14:05 +0300

    update

--- main.js
+++ main.js
@@ -1,68 +1,73 @@
-
+/*
 var x1 = 0; 
 var y1 = 0;
-var startCoords = alert ( "Start coordinates are:A("+x1+","+y1+")" );
-var x2 = prompt ( "Write the 'x' coordinate" );
-var y2 = prompt ( "Write the 'y' coordinate" );
+var startCoords = alert ("Start coordinates are:A("+x1+","+y1+")");
+var x2 = prompt ("Write the 'x' coordinate");
+var y2 = prompt ("Write the 'y' coordinate");
 
-var result=alert ( "Your coordinates are :B(" + x2 + "," + y2 + ").\n" +
+var result=alert ("Your coordinates are :B(" + x2 + "," + y2 + ").\n" +
   "The distance between points A("+x1+","+y1+")and B("+x2+","+y2+")"+
-  " is :\n"+(x2-x1)+" -by 'X'\n"+(y2-y1)+" -by 'Y'");
-
+  " is :\n"+(x2 - x1)+" -by 'X'\n"+(y2 - y1)+" -by 'Y'");
+*/
 
 //---------------------------------------------------------------------------
+/*
+var name = prompt ("Enter your name" , "");
+var time = prompt ("Enter the time" ,  "");
+var place = prompt ("Enter the place", "");
+var event = prompt ("Enter the type of event" , "");
 
-/*var name= prompt ("Enter your name","");
-var time= prompt("Enter the time", "");
-var place= prompt("Enter the place","");
-var event= prompt("Enter the type of event","");
-
-alert(name+" has a "+ event +" today at "+time+ " somewhere in " +place);
-
+alert (name + " has a " + event + " today at " + time + " somewhere in " + place);
 */
+
 //----------------------------------------------------------------------------
 /*
-for(var i = 1;i <= 10; ++i) 
-{
-    for(var j = 1; j <= i; ++j)
-        document.write(' '+"#"+' '); 
-    document.write('<br>'); 
+for (var i = 1 ; i <= 10 ; ++i){
+
+    for (var j = 1; j <= i; ++j){
+
+        document.write (' ' + "#" + ' ' ); 
+        document.write ('<br>'); 
+    }
 };
 */
 //---------------------------------------------------------------------------
 /*
 var hash ="# # # # #"
 var i=1;
-while (i<8)
-{
- if (!(i % 2)) {
-      console.log(' ' + hash);
-    } else {
-      console.log(hash);
+
+while (i < 8){
+
+  if ( !( i % 2 ) ){
+    console.log (' ' + hash);
+  } 
+  else {
+    console.log (hash);
+  }
+  i++;
 }
-i++;
-}
+
 */
 //---------------------------------------------------------------------------
 /*
-var rand =Math.round(Math.random()*100);
-alert(rand);
+var rand = Math.round (Math.random () * 100);
 
+alert (rand);
 
-while (ask!=rand) {
-var ask = prompt ("Please guess the number from 1 to 100"); 
+while (ask != rand) {
 
-if (rand>ask) {
-	alert("Our number is higher, try again");
-	
-}
-else if (rand<ask) {
-	alert("Our number is lower, try again");
-	
-}
+    var ask = prompt ("Please guess the number from 1 to 100"); 
 
-else {
-  alert("You win");
-}
+    if (rand > ask) {
+      alert ("Our number is higher, try again");
+      }
+
+    else if (rand<ask) {
+      alert ("Our number is lower, try again");
+    }
+
+    else {
+      alert("You win");
+    }
 }
 */
